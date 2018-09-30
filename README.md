44c7cb1983247af83dfdc84a0d777ffd0bd406b7
Wilson <wilsonhjda@gmail.com>
2018-09-30 14:52:42 +0800

    Added file data API

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 
-# API Project: File Metadata Microservice for freeCodeCamp
+# API Project: File Metadata Microservice
 
-###    User stories:
-1. I can submit a form that includes a file upload.
-2. The from file input field  has the "name" attribute set to "upfile". We rely on this in testing.
-3. When I submit something, I will receive the file name and size in bytes within the JSON response
+### Live site:
+* https://grateful-barracuda.glitch.me/
 
 ### Usage :
-* Go to the main page, and upload a file using the provided form.
-
-### Hint:
-* To handle the file uploading you should use the [multer](https://www.npmjs.com/package/multer) npm package.
+* Upload a file and get back the name, type and size of the file in json.
--- server.js
+++ server.js
@@ -2,22 +2,49 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var upload = multer()
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
+     res.sendFile(__dirname + '/views/index.html');
   });
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), wrapAsync(async function(req, res) {
+  await new Promise(resolve => setTimeout(() => resolve(), 50));
+  let file = req.file;
+  if (file == undefined) {
+    throw new Error('File missing');
+  }
+  else {
+    return res.json({name: file.originalname, type: file.mimetype, size: file.size})
+  }
+}));
+
+//Error handler
+app.use(function handleMulterError(error, req, res, next) {
+  if (error instanceof multer.MulterError) {
+    return res.status(400).json({message: error.message});
+  }
+  next(error);
+})
+
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
+
+function wrapAsync(fn) {
+  return function(req, res, next) {
+    fn(req, res, next).catch(next);
+  };
+}
